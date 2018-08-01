6a58268d2d202c951400727ac4c99fa8d2a48cae
Shakillia Bobo <sbobofcs@gmail.com>
2018-08-01 10:23:49 -0400

    updated answer

--- 1-average.js
+++ 1-average.js
@@ -1,16 +1,15 @@
 // Problem #1
 // Find the average `price` of all items in the `data` array.
 
-let sum = 0;
+// let sum = 0;
 // let i = 0;
 //Sum of all items divided by the length of the array
 // for (let i = 0; i < data.length; i++){
 //  sum = sum + data[i].price;
 // }
-const sum = data.reduce(function(sum, 0) {
-  return x.price + sum
-}, 0) // starting value
-console.log(sum);
+const sum = data.reduce(function(sum, x) {
+  return sum + x.price
+}, 0)
 
 // change this from null to be the final average price of all items.
 const average = sum/data.length;
